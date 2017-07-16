aecb230d3b00caa4c63426963434c919cc38a34b
ramanandank <ramanandank@vmware.com>
2017-07-16 10:19:52 +0530

    test access token  fix12 commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import requests
 
+
 app = Flask(__name__)
 
 def ResturentListResponse(user_id):
@@ -9,32 +10,8 @@ def ResturentListResponse(user_id):
                    "id": user_id
                  },
                  "message": {
-                   "attachment": {
-                     "type": "template",
-                     "payload": {
-                       "template_type": "generic",
-                       "elements": [
-                         {
-                           "title": "Welcome to Peters Hats",
-                           "image_url": "https://petersfancybrownhats.com/company_image.png",
-                           "subtitle": "We have got the right hat for everyone.",
-                           "buttons": [
-                             {
-                               "type": "web_url",
-                               "url": "https://petersfancybrownhats.com",
-                               "title": "View Website"
-                             },
-                             {
-                               "type": "postback",
-                               "title": "Start Chatting",
-                               "payload": "DEVELOPER_DEFINED_PAYLOAD"
-                             }
-                           ]
-                         }
-                       ]
-                     }
-                   }
-                 }
+                 "text": "hello, world!"
+                  }
            }
     
 
@@ -45,23 +22,23 @@ ACTION_FOOD_SELECTED_FROM_RESTAURANTS = "order placed for burger from kfc"
 
 @app.route("/swiggyBot", methods=["POST"])
 def get_nearbyresturent():
-	if not request.json:
-		abort(400)
-	print(jsonify(request.json))	
-	originalRequest = request.json.get("originalRequest")
-	source = originalRequest.get("source")
-	if source == "facebook":
-		contexts = request.json.get("contexts")
-		results = request.json.get("result")
-		action = results.get("action")
-		data = originalRequest.get("data")
-		recipient = data.get("sender")
-		user_id = recipient.get("id")
-		if action == ACTION_GET_RESTAURENTS:
-                  headers = {'Content-type': 'application/json'}
-                  requests.post(PAGE_ACCESS_TOKEN_URL,data=ResturentListResponse(user_id),headers=headers)
-                  return jsonify(ResturentListResponse(user_id)) ,200
-	return 'Sorry please send from a '
+          if not request.json:
+          	abort(400)
+          print jsonify(request.json)
+          originalRequest = request.json.get("originalRequest")
+          source = originalRequest.get("source")
+          if source == "facebook":
+          	contexts = request.json.get("contexts")
+          	results = request.json.get("result")
+          	action = results.get("action")
+          	data = originalRequest.get("data")
+          	recipient = data.get("sender")
+          	user_id = recipient.get("id")
+          	if action == ACTION_GET_RESTAURENTS:
+                          headers = {'Content-type': 'application/json'}
+                          requests.post(PAGE_ACCESS_TOKEN_URL,data=ResturentListResponse(user_id),headers=headers)
+                          return jsonify(ResturentListResponse(user_id)) ,200
+          return 'Sorry please send from a '
 
 			
 if __name__ == "__main__":
